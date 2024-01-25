da3f60e47fdd0035933823d385264407a879ff65
gwansikk <seorit98@gmail.com>
2024-01-26 02:35:32 +0900

    refactor: component optimize

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,19 +1,19 @@
-import { Fragment, PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 
 import Footer from '../Footer/Footer';
 import Nav from '../Nav/Nav';
 
 const Container = ({ children }: PropsWithChildren) => {
   return (
-    <Fragment>
+    <>
       <Nav />
       <div className="min-h-dvh w-full bg-wbs-black">
-        <main className="container m-auto px-6 py-16 text-white">
+        <main className="container m-auto px-6 py-8 text-wbs-white sm:pt-20">
           {children}
         </main>
       </div>
       <Footer />
-    </Fragment>
+    </>
   );
 };
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,18 @@
 import { FaRssSquare } from 'react-icons/fa';
 
+import { APP_NAME } from '@/src/constants/environment';
+
 const Footer = () => {
   return (
-    <footer className="bg-zinc-900 py-5 text-sm text-white">
+    <footer className="bg-zinc-900 py-5 text-sm text-wbs-white">
       <div className="container m-auto flex justify-between px-6">
         <div className="space-x-1">
-          <span>© WebBlogService.</span>
+          <span>© {APP_NAME}.</span>
           <span>•</span>
           <span>Powered By WebBlogService</span>
         </div>
         <div>
-          <FaRssSquare className="h-5 w-5" />
+          <FaRssSquare className="h-5 w-5 cursor-pointer hover:text-white" />
         </div>
       </div>
     </footer>

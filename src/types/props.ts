f2a31716cd4cd698626b6b59e2a566b5a0ad3311
gwansikk <seorit98@gmail.com>
2024-01-26 02:08:07 +0900

    feat: Props type

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,1 +1,7 @@
+import { ReactNode } from 'react';
+
 export type PropsWithClassName<P = unknown> = P & { className?: string };
+export type Props<P = unknown> = P & {
+  className?: string;
+  children?: ReactNode;
+};

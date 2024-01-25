919eec879636346df60762b113423237a52b7f96
gwansikk <seorit98@gmail.com>
2024-01-26 02:33:42 +0900

    feat: get-posts

---
+++ src/lib/get-posts.ts
@@ -0,0 +1,40 @@
+import { cache } from 'react';
+
+import fs from 'fs/promises';
+import matter from 'gray-matter';
+import path from 'path';
+
+import type { Post } from '../types/post';
+
+export const getPosts = cache(async () => {
+  const posts = await fs.readdir('./posts/');
+
+  const data = await Promise.all(
+    posts
+      .filter(
+        (file) => path.extname(file) === '.mdx' || path.extname(file) === '.md',
+      )
+      .map(async (file) => {
+        const filePath = `./posts/${file}`;
+        const postContent = await fs.readFile(filePath, 'utf8');
+        const { data, content } = matter(postContent);
+
+        if (data.published === false) {
+          return null;
+        }
+
+        return { ...data, body: content } as Post;
+      }),
+  );
+
+  return data.filter((post) => post !== null) as Post[];
+});
+
+export async function getPost(slug: string): Promise<Post | null> {
+  const posts = await getPosts();
+  const findSlug = decodeURIComponent(slug.replace(/-/g, ' '));
+
+  return posts.find((post) => post?.title === findSlug) || null;
+}
+
+export default getPosts;

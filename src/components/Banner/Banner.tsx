5a7b3c107921b0445da6edd099ebd6788482fe0e
gwansikk <seorit98@gmail.com>
2024-01-26 02:36:21 +0900

    refactor: add link to banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,22 +1,29 @@
 import { ReactNode } from 'react';
 
+import { PATH_FINDER } from '@/src/constants/path';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface BannerProps {
+  to: string;
   src: string;
   alt: string;
   children: ReactNode;
 }
 
-const Banner = ({ src, alt, children }: BannerProps) => {
+const Banner = ({ to, src, alt, children }: BannerProps) => {
   return (
-    <div className="hidden cursor-pointer rounded-lg bg-none p-2 transition-colors hover:bg-zinc-900 sm:block">
+    <Link
+      href={PATH_FINDER.POST(to)}
+      className="hidden cursor-pointer rounded-lg bg-none p-2 transition-colors hover:bg-zinc-900 sm:block"
+    >
       <div className="relative flex h-60 items-center overflow-hidden rounded-lg shadow-2xl shadow-zinc-800/50">
         <Image
           src={src}
           alt={alt}
           width={1270}
           height={720}
+          priority
           className="h-full w-full rounded-lg border border-zinc-800 object-cover"
         />
         <div className="pointer-events-none absolute inset-0 h-full w-full rounded-lg bg-gradient-to-t from-black/50 via-black/30" />
@@ -24,7 +31,7 @@ const Banner = ({ src, alt, children }: BannerProps) => {
           {children}
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

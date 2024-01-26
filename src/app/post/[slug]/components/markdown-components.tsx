7b67425285957060f71b9d4c79112b1f26f1cc5f
gwansikk <seorit98@gmail.com>
2024-01-26 20:14:46 +0900

    feat: add bright

--- src/app/post/[slug]/components/markdown-components.tsx
+++ src/app/post/[slug]/components/markdown-components.tsx
@@ -1,3 +1,6 @@
+import { Code } from 'bright';
 import { MDXComponents } from 'mdx/types';
 
-export const mdxComponents: MDXComponents = {};
+export const mdxComponents: MDXComponents = {
+  pre: Code,
+};

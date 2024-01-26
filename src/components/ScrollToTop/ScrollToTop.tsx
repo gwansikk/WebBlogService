67a75d351eea717f541ab7398eed5533ebaeaaa8
gwansikk <seorit98@gmail.com>
2024-01-26 23:50:35 +0900

    fix: scroll to top

---
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { usePathname } from 'next/navigation';
+
+const ScrollToTop = () => {
+  const pathname = usePathname();
+
+  // path가 변경되면 가장 위로 스크롤
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return <div></div>;
+};
+
+export default ScrollToTop;

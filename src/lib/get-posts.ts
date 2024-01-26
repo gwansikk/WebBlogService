80d3c77e0e1d5ef5850792780441fbcdafd1dec0
gwansikk <seorit98@gmail.com>
2024-01-26 20:15:45 +0900

    refactor: get-posts 날짜 정렬

--- src/lib/get-posts.ts
+++ src/lib/get-posts.ts
@@ -4,25 +4,27 @@ import fs from 'fs/promises';
 import matter from 'gray-matter';
 import path from 'path';
 
-import type { Post } from '../types/post';
+import { Post } from '@type/post';
+
+import { APP_EXT, APP_PATH } from '@constants/environment';
 
 export const getPosts = cache(async () => {
-  const posts = await fs.readdir(path.join(process.cwd(), 'posts'));
+  // process.cwd()는 vercel에서는 /var/task, 로컬에서는 프로젝트 경로
+  // https://vercel.com/guides/loading-static-file-nextjs-api-route
+  const posts = await fs.readdir(path.join(process.cwd(), APP_PATH));
 
   const data = await Promise.all(
     posts
-      .filter(
-        (file) => path.extname(file) === '.mdx' || path.extname(file) === '.md',
-      )
+      .filter((file) => APP_EXT.includes(path.extname(file) as any))
       .map(async (file) => {
-        const filePath = `posts/${file}`;
+        const filePath = path.join(APP_PATH, file);
         const postContent = await fs.readFile(
           path.join(process.cwd(), filePath),
           'utf8',
         );
         const { data, content } = matter(postContent);
-
-        if (data.published === false) {
+        // private 게시글은 null로 처리합니다. 미지정시 public으로 간주됩니다.
+        if (data.private === true) {
           return null;
         }
 
@@ -30,7 +32,10 @@ export const getPosts = cache(async () => {
       }),
   );
 
-  return data.filter((post) => post !== null) as Post[];
+  // private 게시글 제외합니다.
+  const publicData = data.filter((post) => post !== null) as Post[];
+  // 날짜 순으로 정렬 후 반환
+  return publicData.sort((a, b) => (a.date < b.date ? 1 : -1));
 });
 
 export async function getPost(slug: string): Promise<Post | null> {

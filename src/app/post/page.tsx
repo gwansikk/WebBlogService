303e2451d9820df0d971dfae0da77fe8a95bf551
gwansikk <seorit98@gmail.com>
2024-01-26 20:19:21 +0900

    refactor: PostPage mocks

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -4,34 +4,41 @@ import { TiPen } from 'react-icons/ti';
 
 import Image from 'next/image';
 
-import data from '../../mocks/data/post.json';
-
-export default function Page() {
-  const { image, category, title, description, date, time, writer } = data;
+export default function PostPage() {
+  const mocks = {
+    image:
+      'https://images.unsplash.com/photo-1706045569094-3d7c366ba5ef?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8',
+    tags: ['tag1'],
+    title: 'Title',
+    description: 'Description',
+    date: 'Date',
+    time: 'Time',
+    writer: 'Writer',
+  };
 
   return (
     <>
       <Image
-        src={image}
-        alt={title}
+        src={mocks.image}
+        alt={mocks.title}
         width={672}
         height={448}
         priority
         className="w-full rounded-lg border border-zinc-800 object-cover"
       />
       <div className="my-10">
-        <p className="text-sm">{category}</p>
-        <h1 className="text-4xl font-bold">{title}</h1>
-        <p>{description}</p>
+        <p className="text-sm">{mocks.tags.join(', ')}</p>
+        <h1 className="text-4xl font-bold">{mocks.title}</h1>
+        <p>{mocks.description}</p>
         <div className="mt-2 flex items-center gap-1 text-sm text-gray-400">
           <CiCalendarDate />
-          <span>{date}</span>
+          <span>{mocks.date}</span>
           <span>•</span>
           <MdOutlineTimer />
-          <span>{time}분</span>
+          <span>{mocks.time}</span>
           <span>•</span>
           <TiPen />
-          <span>{writer}</span>
+          <span>{mocks.writer}</span>
         </div>
       </div>
     </>

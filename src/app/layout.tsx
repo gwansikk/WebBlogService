3c3339a2eb6db233a37ebd04769a4cd76fa31434
gwansikk <seorit98@gmail.com>
2024-01-26 20:17:40 +0900

    refactor: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { APP_DESCRIPTION, APP_NAME } from '@constants/environment';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'WebBlogService',
-  description: 'Create your own blog more easily!',
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
@@ -17,8 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
-        {children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

ab7bddec2a5de66f83b87c1d608537754ab9de62
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-19 23:44:27 +0900

    chore: improve SocialIcon type in web config

--- app/constants/environment.ts
+++ app/constants/environment.ts
@@ -1,5 +1,3 @@
-import exp from 'constants';
-
 import config from '../../wbs.config';
 
 export const APP_NAME = config?.name;
@@ -7,5 +5,4 @@ export const APP_SITE = config?.site;
 export const APP_PATH = config?.path;
 export const APP_EXT = config?.ext;
 export const APP_DESCRIPTION = config?.metadata?.description;
-export const APP_SOCIAL_ICON = config?.socialLinks?.icon;
-export const APP_SOCIAL_LINK = config?.socialLinks?.link;
+export const APP_SOCIAL_LINK = config?.socialLinks;

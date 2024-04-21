8e90a759753521e9f90c495a73428b18d4888c6a
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-22 00:00:18 +0900

    feat: add post-head component

---
+++ app/(subpages)/post/[slug]/components/post-head.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CiCalendarDate } from 'react-icons/ci';
+import { MdOutlineTimer } from 'react-icons/md';
+
+import { APP_THEME_MAX_WIDTH } from '@constants/environment';
+
+import { formatDate } from '@utils/date';
+
+import Badge from '@components/Badge/Badge';
+
+import Image from 'next/image';
+
+import type { Post } from '@type/post';
+
+interface PostHeadProps extends Omit<Post, 'body'> {
+  time: number;
+}
+
+const PostHead = ({
+  title,
+  date,
+  tags,
+  thumbnail,
+  description,
+  time,
+}: PostHeadProps) => {
+  return (
+    <header className="space-y-4">
+      <Image
+        src={thumbnail}
+        alt={title}
+        width={APP_THEME_MAX_WIDTH}
+        height={APP_THEME_MAX_WIDTH}
+        placeholder="blur"
+        blurDataURL={thumbnail}
+        priority
+        className="w-full rounded-lg border border-zinc-800 object-cover"
+      />
+      <div>
+        <div className="flex gap-2">
+          {tags.map((tag) => (
+            <Badge key={tag}>{tag.toUpperCase()}</Badge>
+          ))}
+        </div>
+        <h1 className="text-4xl font-bold text-white">{title}</h1>
+        <p>{description}</p>
+        <div className="mt-2 flex items-center gap-1 text-sm text-gray-400">
+          <CiCalendarDate />
+          <time dateTime={date.toISOString()}>{formatDate(date)}</time>
+          <span>•</span>
+          <MdOutlineTimer />
+          <span>{time}분</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PostHead;
--- app/(subpages)/post/[slug]/page.tsx
+++ app/(subpages)/post/[slug]/page.tsx
@@ -2,22 +2,28 @@ import getPosts, { getPost } from '@/app/lib/get-posts';
 import { notFound } from 'next/navigation';
 
 import { PostBody } from './components/post-body';
+import PostHead from './components/post-head';
+
+type PostPrams = {
+  params: {
+    slug: string;
+  };
+};
 
 export async function generateStaticParams() {
   const posts = await getPosts();
   return posts.map((post) => ({ slug: post.title.replace(/\s/g, '-') }));
 }
 
-export default async function PostPage({
-  params,
-}: {
-  params: {
-    slug: string;
-  };
-}) {
+export default async function PostPage({ params }: Readonly<PostPrams>) {
   const post = await getPost(params.slug);
   // 게시글을 못찾을 경우 notFound
   if (!post) return notFound();
 
-  return <PostBody>{post?.body}</PostBody>;
+  return (
+    <>
+      <PostHead {...post} time={10} />
+      <PostBody>{post?.body}</PostBody>
+    </>
+  );
 }
